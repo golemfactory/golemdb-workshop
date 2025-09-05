dc15a4dcdf73b2da8e67362549552d5f509db666
sieci <sieciech@gmail.com>
2025-09-05 14:54:15 +0200

    More elaborate sample

--- excercise/backend-ts/src/index.ts
+++ excercise/backend-ts/src/index.ts
@@ -1,5 +1,19 @@
-import { Hono } from 'hono'
+import 'dotenv/config'
+import {Hono} from 'hono'
+import jsLogger, {ILogger} from "js-logger";
 
+// Configure logger
+jsLogger.useDefaults();
+jsLogger.setLevel(jsLogger.DEBUG);
+jsLogger.setHandler(jsLogger.createDefaultHandler({
+  formatter: function(messages, context) {
+    // prefix each log message with a timestamp.
+    messages.unshift(`[${new Date().toISOString()} ${context.level.name}]`);
+  }
+}));
+const log: ILogger = jsLogger.get("myLogger");
+
+// Create web server using Hono
 const app = new Hono()
 
 interface DiceThrow {
@@ -7,31 +21,55 @@ interface DiceThrow {
   sum: number
 }
 
+// In-memory storage for dice throws
 const data: DiceThrow[] = []
 
 app.get('/', (c) => {
+  log.debug("Root endpoint called");
   return c.text('Hello on Golem DB Workshop!')
 })
 
-app.get('/api/v1/me', (c) => {
+app.get('/api/v1/me', async (c) => {
+  log.debug('Requested player name:', process.env.PLAYER_NAME || 'default');
   return c.json(process.env.PLAYER_NAME || 'default')
 })
 
-app.get('/api/v1/throws', (c) => {
+app.get('/api/v1/throws', async (c) => {
+  log.debug("Returning throws:", data);
   return c.json(data)
 })
 
-app.post('/api/v1/throws', (c) => {
+app.post('/api/v1/throws', async (c) => {
+  log.debug("Received request to roll dice...");
   const roll = Array.from({length: 5}, () => Math.floor(Math.random() * 6) + 1);
   const diceThrow: DiceThrow = {
     dice: roll,
     sum: roll.reduce((a, b) => a + b, 0)
   };
   data.push(diceThrow);
+  log.debug("Dice rolled:", diceThrow);
   return c.json(diceThrow);
 })
 
-export default {
-  port: 8000,
-  fetch: app.fetch,
+async function initBeforeServerStarts() {
+  log.info("Connecting to Golem DB client...");
+  // Fill your initialization code here
 }
+
+initBeforeServerStarts().then(
+  () => {
+    const port = 8000;
+    log.info(`Starting server at http://localhost:${port}`);
+    Bun.serve({
+        idleTimeout: 30,
+        port,
+        fetch: app.fetch,
+      }
+    );
+  }
+).catch(
+  (e) => {
+    console.error(e)
+    process.exit(1)
+  }
+)
