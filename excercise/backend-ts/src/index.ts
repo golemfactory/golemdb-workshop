eb0e3ec7f33a0636144693cf21f4708c9e1c9bf5
sieci <sieciech@gmail.com>
2025-09-05 15:11:11 +0200

    Added linter and formatter

--- excercise/backend-ts/src/index.ts
+++ excercise/backend-ts/src/index.ts
@@ -1,65 +1,70 @@
 //You can put your environment variables in a .env file
-import 'dotenv/config'
+import "dotenv/config";
 //Hono is a web framework similar to Express
-import {Hono} from 'hono'
-import jsLogger, {ILogger} from "js-logger";
+import { Hono } from "hono";
+import jsLogger, { ILogger } from "js-logger";
 
 // Configure logger for convenience
 jsLogger.useDefaults();
 jsLogger.setLevel(jsLogger.DEBUG);
-jsLogger.setHandler(jsLogger.createDefaultHandler({
-  formatter: function (messages, context) {
-    // prefix each log message with a timestamp.
-    messages.unshift(`[${new Date().toISOString()} ${context.level.name}]`);
-  }
-}));
+jsLogger.setHandler(
+  jsLogger.createDefaultHandler({
+    formatter: function (messages, context) {
+      // prefix each log message with a timestamp.
+      messages.unshift(`[${new Date().toISOString()} ${context.level.name}]`);
+    },
+  }),
+);
 const log: ILogger = jsLogger.get("myLogger");
 
 // Create web server using Hono
-const app = new Hono()
+const app = new Hono();
 
 interface DiceThrow {
-  dice: number[]
-  sum: number
+  dice: number[];
+  sum: number;
 }
 
 // In-memory storage for dice throws
-const data: DiceThrow[] = []
+const data: DiceThrow[] = [];
 
-app.get('/', (c) => {
+app.get("/", (c) => {
   log.debug("Root endpoint called");
-  return c.text('Hello on Golem DB Workshop!')
-})
+  return c.text("Hello on Golem DB Workshop!");
+});
 
-app.get('/api/v1/me', async (c) => {
-  log.debug('Requested player name:', process.env.PLAYER_NAME || 'default');
-  return c.json(process.env.PLAYER_NAME || 'default')
-})
+app.get("/api/v1/me", async (c) => {
+  log.debug("Requested player name:", process.env.PLAYER_NAME || "default");
+  return c.json(process.env.PLAYER_NAME || "default");
+});
 
-app.get('/api/v1/throws', async (c) => {
+app.get("/api/v1/throws", async (c) => {
   log.debug("Returning throws:", data);
-  return c.json(data)
-})
+  return c.json(data);
+});
 
-app.post('/api/v1/throws', async (c) => {
+app.post("/api/v1/throws", async (c) => {
   log.debug("Received request to roll dice...");
-  const roll = Array.from({length: 5}, () => Math.floor(Math.random() * 6) + 1);
+  const roll = Array.from(
+    { length: 5 },
+    () => Math.floor(Math.random() * 6) + 1,
+  );
   const diceThrow: DiceThrow = {
     dice: roll,
-    sum: roll.reduce((a, b) => a + b, 0)
+    sum: roll.reduce((a, b) => a + b, 0),
   };
   data.push(diceThrow);
   log.debug("Dice rolled:", diceThrow);
   return c.json(diceThrow);
-})
+});
 
 async function initBeforeServerStarts() {
   log.info("Connecting to Golem DB client...");
   // Fill your initialization code here
 }
 
-initBeforeServerStarts().then(
-  () => {
+initBeforeServerStarts()
+  .then(() => {
     // Start the server on port 8000, if you change, fix also in frontend
     const port = 8000;
     // Default bun timeout is 10s which is sometimes not enough, feel free to adjust
@@ -67,15 +72,12 @@ initBeforeServerStarts().then(
     log.info(`Starting server at http://localhost:${port}`);
     // Serve using Bun's built-in server
     Bun.serve({
-        idleTimeout,
-        port,
-        fetch: app.fetch,
-      }
-    );
-  }
-).catch(
-  (e) => {
+      idleTimeout,
+      port,
+      fetch: app.fetch,
+    });
+  })
+  .catch((e) => {
     log.error(e);
-    process.exit(1)
-  }
-)
+    process.exit(1);
+  });
